a32b704d2f7ad2a76feda8dd82225f2dda8855ce
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-12-08 18:39:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Route , Routes } from 'react-router-dom'
 import Login from "./Screens/Login/Login";
+import SignUp from "./Screens/Signup/SignUp";
 
 const App = () => {
   return (
       <Routes>
        <Route path="/" index element={<Login />} />
+       <Route path="/signup" index element={<SignUp />} />
       
     
       </Routes>
--- src/Screens/Signup/SignUp.jsx
+++ src/Screens/Signup/SignUp.jsx
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
 import Logo from "../assets/logo.jpeg"
-import "./Login.css"
+import "./SignUp.css"
 import Header from '../../Views/Header/Header'
 import Footer from '../../Views/Footer/footer'
 import { useNavigate } from 'react-router-dom'
 
-const Login = ()=>{
+const SignUp = ()=>{
 
     const [userName,setUserName] = useState("");
     const [password,setPassword] = useState("");
@@ -25,27 +25,41 @@ const Login = ()=>{
    
         <div className='s1c1' >
           <img src={Logo} className='login-logo' alt = "capitalone logo" />
-          <h6 className='s1t1'>Sign in</h6>
+          <h6 className='s1t1'>Sign Up</h6>
 
-          <p className='s1t3'>Username</p>
+          <p className='s1t3'>First Name</p>
           <input  onChange={(e)=>setUserName(e.target.value)} type='text' className='s1I1' />
 
+         
+          <p className='s1t3'>Last Name</p>
+          <input  onChange={(e)=>setUserName(e.target.value)} type='text' className='s1I1' />
+
+
+          <p className='s1t3A'>Email(User Name)</p>
+          <input  onChange={(e)=>setUserName(e.target.value)} type='text' className='s1I1' />
+
+          
+          <p className='s1t3B'>Social Security Number(SSN)</p>
+          <input  onChange={(e)=>setUserName(e.target.value)} type='text' className='s1I1' />
 
           <p className='s1t4'>Password</p>
           <input onChange={(e)=>setPassword(e.target.value)} type='password' className='s1I2' />
 
+          <p className='s1t3A'>Confirm Password</p>
+          <input onChange={(e)=>setPassword(e.target.value)} type='password' className='s1I2' />
+
          <div className='s1c2'> 
          <input className='s1I3' type="checkbox"  />
-         <p className='s1t5'>Remember Me</p>
+         <p className='s1t5'>Keep Me Signed In</p>
          </div>
 
          <div onClick={()=>SignIn()} className='s1c3'>
-            <p className='s1t6'>Sign in</p>
+            <p className='s1t6'>Sign Up</p>
          </div>
  
-         <h6 className='s1t7'>Forgot Username or Password?</h6>
+         <h6 onClick = {()=>navigate("/")}className='s1t7'>Already have an account? Sign In</h6>
 
-         <h6 onClick={()=>navigate("signup")} className='s1t8'>Set Up an Online Access?</h6>
+     
         </div>
         <Footer />
         </div>
@@ -56,4 +70,4 @@ const Login = ()=>{
 }
 
 
-export default Login;
+export default SignUp;
